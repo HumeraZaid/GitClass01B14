0418ad3879a53086fe980ebaaba47650fa5aab7f
HumeraZaid <humerazshelia@gmail.com>
2022-10-23 01:30:26 -0500

    first commit

---
+++ src/class01codes/class01Codes.java
@@ -0,0 +1,13 @@
+package class01codes;
+
+public class class01Codes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Code to launch the nuclear weapon
+		System.out.println("Launch my favorite TV show");
+		
+	}
+
+}
